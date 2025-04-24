497eb5c267ddd723ec4d4f5a694893d225643fd3
DhanushKeloth <dhanushkeloth@gmail.com>
2025-04-24 12:04:08 +0530

    removed address

--- src/components/LogistFooter.jsx
+++ src/components/LogistFooter.jsx
@@ -40,7 +40,7 @@ const LogistFooter = () => {
           {/* Address */}
           <div>
             <h2 className="font-bold text-lg mb-4">Address</h2>
-            <p className="mb-4">2972 Westheimer Rd. Santa Ana, Illinois 85486</p>
+            <p className="mb-4"></p>
             <a href="#" className="flex items-center text-gray-700 hover:text-black">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-5 h-5 mr-2">
                 <circle cx="12" cy="12" r="10"></circle>
